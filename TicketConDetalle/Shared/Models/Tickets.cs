8d24d5d6d9691e7fb743dc1e82f10380308291a0
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-20 09:28:42 -0400

    Agregando modelo Tickets

---
+++ TicketConDetalle/Shared/Models/Tickets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketConDetalle.Shared.Models
+{
+    internal class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "El compo Fecha es requerido")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El campo Cliente ID  es requerido")]
+        public int ClienteId { get; set; }
+        [ForeignKey(nameof(ClienteId))]
+        public Clientes? Cliente { get; set; }        
+
+        [Required(ErrorMessage = "EL campo Solicitado es requerido")]
+        [StringLength(50, ErrorMessage = "El compo Solicitado por supera los 50 caracteres")]
+        public string? SolicitadoPor { get; set; }
+
+        [Required(ErrorMessage = "EL campo Asunto es requerido")]
+        [StringLength(100, ErrorMessage = "El compo Asunto supera los 100 caracteres")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "EL campo Descripcion es requerido")]
+        [StringLength(100, ErrorMessage = "El compo Decripción supera los 100 caracteres")]
+        public string? Descripcion { get; set; }
+
+        [ForeignKey("TicketId")]
+        public ICollection<TicketsDetalle> TicketsDetalles { get; set; } = new List<TicketsDetalle>();
+
+
+    }
+}
