6865fc5ae9a3c75fafdfe1d66871c4d6913a8ce3
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-20 10:53:36 -0400

    Agregando migracion inicial

---
+++ TicketConDetalle/Server/Migrations/20231020144427_Inicial.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketConDetalle.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", maxLength: 40, nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", maxLength: 12, nullable: false),
+                    Celular = table.Column<string>(type: "TEXT", maxLength: 12, nullable: false),
+                    Rnc = table.Column<string>(type: "TEXT", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SolicitadoPor = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Asunto = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TicketsDetalle",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EmisorClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Mensaje = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TicketsDetalle", x => x.DetalleId);
+                    table.ForeignKey(
+                        name: "FK_TicketsDetalle_Clientes_EmisorClienteId",
+                        column: x => x.EmisorClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TicketsDetalle_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ClienteId",
+                table: "Tickets",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketsDetalle_EmisorClienteId",
+                table: "TicketsDetalle",
+                column: "EmisorClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketsDetalle_TicketId",
+                table: "TicketsDetalle",
+                column: "TicketId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TicketsDetalle");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+        }
+    }
+}
