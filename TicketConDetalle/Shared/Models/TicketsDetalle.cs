3641590c0a5cee0a55ebbff6cc4caddae7b11855
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-20 09:30:05 -0400

    Agregando modelo TicketsDetalle

---
+++ TicketConDetalle/Shared/Models/TicketsDetalle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketConDetalle.Shared.Models
+{
+    internal class TicketsDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int TicketId { get; set; }
+        public Clientes Emisor { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
