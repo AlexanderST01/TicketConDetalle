f795778e15a019d35d9fc268ea6c6610658165fa
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-20 15:40:41 -0400

    Agregando NotificationService

--- TicketConDetalle/Client/Program.cs
+++ TicketConDetalle/Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TicketConDetalle.Client;
+using Radzen;
+using Radzen.Blazor;
 
 namespace TicketConDetalle.Client
 {
@@ -11,6 +13,7 @@ namespace TicketConDetalle.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+            builder.Services.AddScoped<NotificationService>();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
