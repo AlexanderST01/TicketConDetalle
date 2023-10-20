e6beb9c5a68166f7954eb7e1d8401ac8f20e2222
AlexanderST01 <137325892+AlexanderST01@users.noreply.github.com>
2023-10-20 16:43:56 -0400

    creando nuevo migracion para quitar error de las tildes

---
+++ TicketConDetalle/Server/Migrations/20231020202002_Agregando acentos al model cliente.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketConDetalle.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandoacentosalmodelcliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefono",
+                table: "Clientes",
+                newName: "Teléfono");
+
+            migrationBuilder.RenameColumn(
+                name: "Direccion",
+                table: "Clientes",
+                newName: "Dirección");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Teléfono",
+                table: "Clientes",
+                newName: "Telefono");
+
+            migrationBuilder.RenameColumn(
+                name: "Dirección",
+                table: "Clientes",
+                newName: "Direccion");
+        }
+    }
+}
